a12b4da1f3e9b3b2ae138410d4aeecd43bb5bb1e
jun9130 <jun.nakama1030@gmail.com>
2023-10-16 23:11:28 +0900

    create

--- index.js
+++ index.js
@@ -12,3 +12,23 @@ const questions = [
 const entered = document.getElementById('entered');
 const remained = document.getElementById('remained');
 const inputText = document.getElementById('inputText');
+
+let remainedTextWords = remained.textContent.split('');
+let enteredTextWords = [];
+
+document.addEventListener('input', (e) => {
+  if (remainedTextWords[0] === e.data) {
+    // 入力済み文字の配列の最後に1文字を追加
+    enteredTextWords.push(remainedTextWords[0]);
+    // 未入力文字の配列の先頭から1文字を削除
+    remainedTextWords.shift();
+
+    console.log('==========================');
+    console.log('入力済み' + enteredTextWords);
+    console.log('未入力' + remainedTextWords);
+    console.log('==========================');
+
+  } else {
+    console.log('不正解');
+  }
+});
